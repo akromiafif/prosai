7d15d58be29bc29f5b2b1ef1844eb98df7be3db2
Afif Akromi <massachusetts48@gmail.com>
2022-02-15 16:14:04 +0700

    convert class to functional component

--- components/KanbanDrag.js
+++ components/KanbanDrag.js
@@ -1,101 +1,96 @@
-import React, { Component } from "react";
-import { useRecoilValue } from "recoil";
+import React, { useEffect, useState } from "react";
 import KanbanItem from "./KanbanItem";
 import KanbanTask from "./KanbanTask";
-import { taskState } from "../src/state";
 import { Task } from "../src/constant";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tasks: Task,
-      isClicked: false,
-    };
-  }
+const KanbanDrag = () => {
+  const [tasks, setTasks] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
 
-  onDragOver = (e) => {
+  useEffect(() => {
+    setTasks(Task);
+    console.log(tasks);
+  }, []);
+
+  const onDragOver = (e) => {
     e.preventDefault();
   };
 
-  onDragStart = (e, id) => {
+  const onDragStart = (e, id) => {
     e.dataTransfer.setData("id", id);
   };
 
-  onDrop = (e, cat) => {
+  const onDrop = (e, cat) => {
     let id = e.dataTransfer.getData("id");
-    let tasks = this.state.tasks.filter((task) => {
+    let newTask = tasks.filter((task) => {
       if (task.id == id) {
         task.category = cat;
       }
       return task;
     });
-
-    this.setState({ ...this.state.tasks, tasks });
+    setTasks(newTask);
   };
 
-  handleAddNew = () => {
-    let val = this.state.isClicked ? false : true;
-    this.setState({ isClicked: val });
+  const handleAddNew = () => {
+    let val = isClicked ? false : true;
+    setIsClicked(val);
   };
 
-  handleClose = (obj) => {
-    this.setState({ isClicked: obj });
+  const handleClose = (obj) => {
+    setIsClicked(obj);
   };
 
-  handleNewTask = (content) => {
-    const task = {};
-    task.name = content;
-    task.bgColor = "#9fa8da";
-    task.category = "backlog";
-    const tasks = [...this.state.tasks, task];
-    this.setState({ tasks });
+  const handleNewTask = (content) => {
+    const newTask = {};
+    newTask.name = content;
+    newTask.bgColor = "#9fa8da";
+    newTask.category = "backlog";
+    const newTask2 = [tasks, newTask];
+    setTasks(newTask2);
   };
 
-  render() {
-    const tasks = {
-      backlog: [],
-      done: [],
-      todo: [],
-    };
+  const tasksLog = {
+    backlog: [],
+    done: [],
+    todo: [],
+  };
 
-    this.state.tasks.forEach((t) => {
-      tasks[t.category].push(
-        <KanbanTask
-          type={t.type}
-          title={t.name}
-          txtType={t.txtType}
-          onDragStart={(e) => this.onDragStart(e, t.id)}
-        />
-      );
-    });
-
-    return (
-      <div className="flex flex-row">
-        {/* {loader} */}
-        <KanbanItem
-          title="Backlog"
-          tasks={tasks}
-          onDragOver={(e) => this.onDragOver(e)}
-          onDrop={(e) => this.onDrop(e, "backlog")}
-        />
-
-        <KanbanItem
-          title="Todo"
-          tasks={tasks}
-          onDragOver={(e) => this.onDragOver(e)}
-          onDrop={(e) => this.onDrop(e, "todo")}
-        />
-
-        <KanbanItem
-          title="Done"
-          tasks={tasks}
-          onDragOver={(e) => this.onDragOver(e)}
-          onDrop={(e) => this.onDrop(e, "done")}
-        />
-      </div>
+  tasks.forEach((t) => {
+    tasksLog[t.category].push(
+      <KanbanTask
+        type={t.type}
+        title={t.name}
+        txtType={t.txtType}
+        onDragStart={(e) => onDragStart(e, t.id)}
+      />
     );
-  }
-}
+  });
 
-export default App;
+  return (
+    <div className="flex flex-row">
+      {/* {loader} */}
+      <KanbanItem
+        title="Backlog"
+        tasks={tasksLog}
+        onDragOver={(e) => onDragOver(e)}
+        onDrop={(e) => onDrop(e, "backlog")}
+      />
+
+      <KanbanItem
+        title="Todo"
+        tasks={tasksLog}
+        onDragOver={(e) => onDragOver(e)}
+        onDrop={(e) => onDrop(e, "todo")}
+      />
+
+      <KanbanItem
+        title="Done"
+        tasks={tasksLog}
+        onDragOver={(e) => onDragOver(e)}
+        onDrop={(e) => onDrop(e, "done")}
+      />
+    </div>
+  );
+};
+
+export default KanbanDrag;
